1b787989eff3d6c24b4b7430b3709a33d48d8126
coder <tsering@Tsering>
2023-02-07 20:45:57 +0545

    Hero Banner Section Complete

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,24 @@
 import "./Banner.scss";
+
+import BannerImg from "../../../assets/banner-img.png"
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div>
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque quod vel voluptatum commodi. Nostrum deleniti voluptatem ratione qui quo cumque totam vel ut repudiandae expedita soluta atque, ipsa ipsam!
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={BannerImg} alt="featured-product"/>
+            </div>
+        </div>
+    </div>;
 };
 
 export default Banner;
