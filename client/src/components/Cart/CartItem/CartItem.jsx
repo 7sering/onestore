b5c5301dc858a5872767ee2b2153a1007326d036
coder <tsering@Tsering>
2023-02-16 08:43:42 +0545

    Cart Component UI Complete

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,29 @@
 import "./CartItem.scss";
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/earbuds-prod-4.webp"
 const CartItem = () => {
-    return <div>Cart item</div>;
+    return <div className="cart-products">
+        <div className="cart-product">
+            <div className="img-container">
+                <img src={prod} alt="product"  />
+            </div>
+
+            <div className="prod-details">
+                <span className="name">O2 Earbuds 2.0</span>
+                <MdClose className="close-btn"/>
+                <div className="quantity-buttons">
+                    <span>-</span>
+                    <span>3</span>
+                    <span>+</span>
+                </div>
+                <div className="text">
+                    <span>3</span>
+                    <span>x</span>
+                    <span className="highlight">$35.99</span>
+                </div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default CartItem;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -13,6 +13,7 @@ import { Context } from "../../utils/context";
 import "./Header.scss";
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -28,25 +29,29 @@ const Header = () => {
   }, []);
 
   return (
-    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center">ONESTORE.</div>
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <span className="cart-icon">
-            {" "}
-            <CgShoppingCart />
-            <span>5</span>
-          </span>
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categories</li>
+          </ul>
+          <div className="center">ONESTORE.</div>
+          <div className="right">
+            <TbSearch />
+            <AiOutlineHeart />
+            <span className="cart-icon" onClick={() => setShowCart(true)}>
+            
+              <CgShoppingCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {showCart && <Cart setShowCart={setShowCart}/>}
+    </>
   );
 };
 
