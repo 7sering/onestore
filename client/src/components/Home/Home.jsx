18257c8e6041fb27adbe50928a33a5c2ac879782
coder <tsering@Tsering>
2023-03-05 19:23:59 +0545

    API Setup Complet

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,20 +1,33 @@
 import "./Home.scss";
+import { useEffect } from "react";
 
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
 
+
+import { fetchDataFromApi } from "../../utils/api";
+
 const Home = () => {
-    return <>
-        <Banner/>
-        <div className="main-content">
-            <div className="layout">
-                <Category/>
-                <Products headingText="Popular Products"/>
-            </div>
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories?populate=*").then(res => console.log(res));
+  };
+
+  return (
+    <>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+          <Products headingText="Popular Products" />
         </div>
-    </>;
-    
+      </div>
+    </>
+  );
 };
 
 export default Home;
